b355d7ab0623eed25fd9211a9a96ccbafb15fee7
Azlan-saja <azlansaja19@gmail.com>
2023-08-03 20:07:29 +0700

    membuat file home.dart

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:aplikasi_5simic1/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Aplikasi 5SIMIC1",
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      home: const MyDashboard(),
+    );
+  }
+}
