de035bdfd4f9277d1a97d4fd0578cf6971712093
Azlan-saja <azlansaja19@gmail.com>
2023-08-18 20:31:40 +0700

    Design Layer Login Final

--- lib/login.dart
+++ lib/login.dart
@@ -6,30 +6,70 @@ class MyLogin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const Text('Aplikasi 5SIMIC1'),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(onPressed: () {}, child: const Text('Google')),
-              ElevatedButton(onPressed: () {}, child: const Text('Facebook'))
-            ],
-          ),
-          const TextField(
-            decoration: InputDecoration(
-              labelText: 'Email',
-              border: UnderlineInputBorder(),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                'A P L I K A S I \n5SIMIC1',
+                style: TextStyle(fontSize: 30),
+                textAlign: TextAlign.center,
+              ),
             ),
-          ),
-          const TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              labelText: 'Password',
-              border: UnderlineInputBorder(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red[800],
+                  ),
+                  onPressed: () {},
+                  child: const Text('Google'),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue[600],
+                  ),
+                  onPressed: () {},
+                  child: const Text('Facebook'),
+                )
+              ],
             ),
-          ),
-        ],
+            const Padding(
+              padding: EdgeInsets.all(18),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: UnderlineInputBorder(),
+                ),
+              ),
+            ),
+            const SizedBox(height: 5),
+            const Padding(
+              padding: EdgeInsets.all(18),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: UnderlineInputBorder(),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Login'),
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              const Text('Belum Punya Akun?'),
+              TextButton(
+                onPressed: () {},
+                child: const Text('Register'),
+              ),
+            ]),
+          ],
+        ),
       ),
     );
   }
