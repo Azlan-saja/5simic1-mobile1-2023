a787ff7664faa1b6b6726af5f3c832cdf49df0dd
Azlan-saja <azlansaja19@gmail.com>
2023-08-24 19:03:33 +0700

    State Manajemen Login

--- lib/login.dart
+++ lib/login.dart
@@ -1,7 +1,18 @@
+import 'package:aplikasi_5simic1/home.dart';
 import 'package:flutter/material.dart';
 
-class MyLogin extends StatelessWidget {
+// a) Ubah menjadi StatefulWidget
+class MyLogin extends StatefulWidget {
   const MyLogin({super.key});
+
+  @override
+  State<MyLogin> createState() => _MyLoginState();
+}
+
+class _MyLoginState extends State<MyLogin> {
+  // b) Buat variabel baru menggunakan TextEditingController()
+  final vEmail = TextEditingController();
+  final vPassword = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -30,35 +41,60 @@ class MyLogin extends StatelessWidget {
                 ),
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue[600],
+                    backgroundColor: Colors.blue[800],
                   ),
                   onPressed: () {},
                   child: const Text('Facebook'),
                 )
               ],
             ),
-            const Padding(
-              padding: EdgeInsets.all(18),
+            Padding(
+              padding: const EdgeInsets.all(18),
               child: TextField(
-                decoration: InputDecoration(
+                // c) Sandingkan variabel vEmail
+                //    dengan inputan email
+                //    menggunakan atribut controller
+                controller: vEmail,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
                   labelText: 'Email',
                   border: UnderlineInputBorder(),
                 ),
               ),
             ),
             const SizedBox(height: 5),
-            const Padding(
-              padding: EdgeInsets.all(18),
+            Padding(
+              padding: const EdgeInsets.all(18),
               child: TextField(
+                // c) Sandingkan variabel vPassword
+                //    dengan inputan password
+                //    menggunakan atribut controller
+                controller: vPassword,
                 obscureText: true,
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                   labelText: 'Password',
                   border: UnderlineInputBorder(),
                 ),
               ),
             ),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                // d) Kondisikan state/data vEmail dan vPassword
+                if (vEmail.text == 'a@g.c' && vPassword.text == '123') {
+                  // Login Berhasil
+                  // Navigasi
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const MyDashboard(),
+                    ),
+                  );
+                } else {
+                  // Login Gagal
+                  const pesan = SnackBar(content: Text('Login Gagal.'));
+                  ScaffoldMessenger.of(context).showSnackBar(pesan);
+                }
+              },
               child: const Text('Login'),
             ),
             Row(mainAxisAlignment: MainAxisAlignment.center, children: [
